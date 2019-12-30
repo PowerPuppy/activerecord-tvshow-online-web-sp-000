a49e61f468ccfc8951a7c57a1e9b317a5faf4b3f
Chris Cheong <chrisxcheong@gmail.com>
2019-12-30 20:14:07 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.find_by(:rating self.highest_rating)
+    Show.find_by(:rating self.highest_rating).first
   end
 
   def self.lowest_rating
@@ -12,7 +12,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    Show.find_by(:rating self.lowest_rating)
+    Show.find_by(:rating self.lowest_rating).first
   end
 
   def self.ratings_sum
